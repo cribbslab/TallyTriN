87117d324087dda504dc0098ddb90f09e49f88c2
Acribbs <a.p.cribbs@gmail.com>
2023-04-19 11:09:17 +0100

    pytsest import working

--- tests/test_import.py
+++ tests/test_import.py
@@ -14,13 +14,11 @@ import os
 import pytest
 
 # define the directories to test
-directories = ['tallytrin/entry.py']
+directories = ['tallytrin', 'pysam', 'pandas']
 
 # define the test function
 @pytest.mark.parametrize('directory', directories)
 def test_imports(directory):
-    for file in os.listdir(directory):
-        if file.endswith('.py') and file != '__init__.py':
-            module_name = file[:-3]
-            module = importlib.import_module(f'{directory}.{module_name}')
-            assert module is not None
+    for file in directory:
+        module = importlib.import_module(directory)
+        assert module is not None
