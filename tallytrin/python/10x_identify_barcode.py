39851340f1893c6cd05d115c3a842423c096bec3
Acribbs <a.p.cribbs@gmail.com>
2023-10-27 14:11:29 +0100

    bug fix with cmimode

--- tallytrin/python/10x_identify_barcode.py
+++ tallytrin/python/10x_identify_barcode.py
@@ -10,6 +10,7 @@ L = logging.getLogger("10x_identify_barcode.py")
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--whitelist", default=None, type=str, help='Name of whitelist file')
+    parser.add_argument("--cmimode", default=0, type=str, help='Run CMI for accuracy evaluation')
     parser.add_argument("--infile", default=None, type=str, help='Nanopore infile FASTQ file')
     parser.add_argument("--outfile", default=None, type=str, help='Name for output FASTQ files')
     parser.add_argument("--barcode", default=16, type=int, help='Length of barcode required')
